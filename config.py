7a1453253babfad22dde3949d7324b1eb48e53c9
masterbbb001 <masterbbb001@gmail.com>
2026-05-24 09:27:47 +0500

    Change CHANNEL_ID and ORDERS_CHANNEL_ID to usernames

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-5216202221"))
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "@Globus_kanstovar_xiva")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "-5216202221"))
-ORDERS_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-5216202221"))
+ORDERS_CHANNEL_ID = os.environ.get("CHANNEL_ID", "@Globus_kanstovar_xiva")
